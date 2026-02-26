c5c156bb4498c27ac1f719916a90e8d559b3be04
Tapan <shikaritapan@gmail.com>
2026-02-26 19:01:12 +0530

    project name chnage

--- src/main/java/com/api/docker/demo/DeckerProjectApplication.java
+++ src/main/java/com/api/docker/demo/DeckerProjectApplication.java
@@ -2,7 +2,8 @@ package com.api.docker.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
